5b3cb945aa3d720f25ecb371e6827a07216a5154
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-03 10:05:57 -0700

    added seeds file logic

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,25 @@
--- In this file, write insert queries to populate the burgers table with at least three entries.
--- Run the schema.sql and seeds.sql files into the mysql server from the command line
+-- In this file, write insert queries to populate the burgers 
+-- burgers table with at least three entries.
+INSERT INTO burgers_table (burger_name, devoured) VALUES('The Western Burger', false);
+INSERT INTO burgers_table (burger_name, devoured) VALUES('The Surf-n-Turf Burger', true);
+INSERT INTO burgers_table (burger_name, devoured) VALUES('The Black Bean Burger', false);
+-- Run the schema.sql and seeds.sql files into the 
+-- mysql server from the command line
+
 -- Now you're going to run these SQL files.
 
 
 
 -- Make sure you're in the db folder of your app.
+
 -- Start MySQL command line tool and login: mysql -u root -p.
--- With the mysql> command line tool running, enter the command source schema.sql. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
--- Now insert the entries you defined in seeds.sql by running the file: source seeds.sql.
+
+-- With the mysql> command line tool running, enter the 
+-- command source schema.sql. This will run your schema file 
+-- and all of the queries in it -- in other words, you'll be 
+-- creating your database.
+
+-- Now insert the entries you defined in seeds.sql 
+-- by running the file: source seeds.sql.
+
 -- Close out of the MySQL command line tool: exit.
