2b39c2f001c6c27dc9c5e5916bc20d1e604c0501
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-03 09:56:34 -0700

    created schema logic

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,23 @@
 
 
 -- Create the burgers_db.
+    CREATE DATABASE burgers_db;
+
+
 -- Switch to or use the burgers_db.
+    USE burgers_db;
+
 -- Create a burgers table with these fields:
-
-
-
 -- id: an auto incrementing int that serves as the primary key.
-
 -- burger_name: a string.
-
 -- devoured: a boolean.
+    CREATE TABLE  burgers_table (
+        id int NOT NULL AUTO_INCREMENT,
+        burger_name VARCHAR(255) NOT NULL,
+        devoured boolean NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+
+
+
