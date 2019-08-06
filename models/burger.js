da1e48534eb6bc0e5d7af054cd3416f7b57f5aca
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-05 21:36:14 -0700

    stopping point for night orm

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,6 @@
 // import orm.js
+var orm = require("../config/orm.js");
 
 // create the code that will call the ORM functions using burger specific input for the ORM.
 // Export at the end of the burger.js file.
+module.exports = burger;
