5e4e1b8583f3c4fab6c957d7811f27a8843e5543
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-06 21:01:16 -0700

    orm sql helper function

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,21 @@
 // Import (require) connection.js into orm.js
+var connection = require("../config/connection.js");
 
-// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+// In the orm.js file, create the methods that will execute
+// the necessary MySQL commands in the controllers. 
+// These are the methods you will need to use in order to 
+// retrieve and store data in your database.
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
 
 
 // selectAll()
